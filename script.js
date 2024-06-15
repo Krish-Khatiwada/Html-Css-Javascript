d5106e7798323290ad816c808112796b86b1955a
Krish Khatiwada <krishkhatiwada125@gmail.com>
2024-06-15 16:18:30 +0200

    modified

--- script.js
+++ script.js
@@ -1,2 +1,25 @@
-alert ("Hello world")
-prompt("hello world")
+let K = "vague"
+let L = 2
+console.log (K + L)
+
+const a = {
+    name: "hero",
+    class: 10,
+    section: "D",
+    Profession: "Doctor"
+}
+a["friend"] = "zero"
+console.log (a)
+
+const b = 3.14
+// b = 3.1415 // b is already decleared
+
+const dict = {
+    sierra: "a chain of hills or mountains, the peaks of which suggest the teeth of a saw.",
+    hallucinate: " to produce false information contrary to the intent of the user and present it as if true and factual.",
+    repudiate: "to reject or refuse to accept something",
+    deny: " to say something is not true",
+    monkey: "a small to medium-sized primate that typically has a long tail, most kinds of which live in trees in tropical countries."
+}
+
+console.log (dict.monkey)
